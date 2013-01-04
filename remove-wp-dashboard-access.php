3b54ab2c40b21674845c3affbe583a28998776f4
Drew Jaynes <info@drewapicture.com>
2013-01-04 12:59:35 -0700

    Hide menus and Toolbar items with hide_menus and hide_toolbar_items methods, add profile.php check to dashboard_redirect method.

--- remove-wp-dashboard-access.php
+++ remove-wp-dashboard-access.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Remove Dashboard Access for Non-Admins
+Plugin Name: Remove Dashboard Access
 Plugin URI: http://www.werdswords.com
 Description: Removes Dashboard access for non-admin users
 Version: 0.4
@@ -17,6 +17,8 @@ class Remove_Dashboard_Access {
 	 */
 	function __construct() {
 		add_action( 'admin_init', array( $this, 'dashboard_redirect' ) );
+		add_action( 'admin_head', array( $this, 'hide_menus' ) );
+		add_action( 'admin_bar_menu' array( $this, 'hide_toolbar_items' ), 999 );
 	}
 	
 	/**
@@ -29,7 +31,8 @@ class Remove_Dashboard_Access {
 	 * @return null
 	 */
 	function dashboard_redirect() {
-		if ( ! current_user_can( $this->user_cap() ) && ! defined( 'DOING_AJAX' ) ) {
+		global $pagenow;
+		if ( 'profile.php' != $pagenow && ! current_user_can( $this->user_cap() ) && ! defined( 'DOING_AJAX' ) ) {
 			wp_redirect( $this->redirect_url() );
 			exit;
 		}
@@ -55,6 +58,47 @@ class Remove_Dashboard_Access {
 	 */
 	function redirect_url() {
 		return apply_filters( 'rda_redirect_url', home_url() );
+	}
+
+	/**
+	 * Hide Admin Menus
+	 *
+	 * @since 0.5
+	 *
+	 * @return null
+	 */
+	function hide_menus() {
+		?>
+		<style type="text/css">
+		#adminmenuback, #adminmenuwrap {
+			display: none;
+		}
+		.wrap {
+			margin-top: 1.5%;
+		}
+		#wpcontent {
+			margin-left: 2%;
+		}
+		<?php
+	}
+
+	/**
+	 * Hide Toolbar Items
+	 *
+	 * @since 1.0
+	 *
+	 * @param $wp_admin_bar global
+	 * @return null
+	 */
+	function hide_toolbar_items( $wp_admin_bar ) {
+		// global $wp_admin_bar;
+		$args = array( 'about', 'comments', 'new-content' );
+		$nodes = apply_filters( 'rda_toolbar_nodes', $args );
+		foreach ( $nodes as $id ) {
+			$wp_admin_bar->remove_node( $id );
+		}
+		if ( ! is_admin() )
+			$wp_admin_bar->remove_node( 'dashboard' );
 	}
 }
 
