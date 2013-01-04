1528d3866841989a1b5771bbba1bd10af6ae3232
Drew Jaynes <info@drewapicture.com>
2013-01-04 12:35:08 -0700

    Split out capability and redirect URL bits to methods, add docs.

--- remove-wp-dashboard-access.php
+++ remove-wp-dashboard-access.php
@@ -10,15 +10,51 @@ License: GPLv2
 */
 
 class Remove_Dashboard_Access {
+	/**
+	 * Init
+	 *
+	 * @since 0.5
+	 */
 	function __construct() {
-		add_action( 'admin_init', array( $this, 'rda_redirect' ) );
+		add_action( 'admin_init', array( $this, 'dashboard_redirect' ) );
 	}
 	
-	function rda_direct() {
-		if ( ! current_user_can( 'manage_options' ) && ! defined( 'DOING_AJAX' ) ) {
-			wp_redirect( home_url() );
+	/**
+	 * Dashboard Redirect
+	 *
+	 * @since 0.1
+	 *
+	 * @uses current_user_can() to determine user capability for accessing the Dashboard
+	 * @uses wp_redirect() to redirect unauthorized users
+	 * @return null
+	 */
+	function dashboard_redirect() {
+		if ( ! current_user_can( $this->user_cap() ) && ! defined( 'DOING_AJAX' ) ) {
+			wp_redirect( $this->redirect_url() );
 			exit;
 		}
+	}
+
+	/**
+	 * User Capability
+	 *
+	 * @since 0.5
+	 *
+	 * @return string user capability
+	 */
+	function user_cap() {
+		return apply_filters( 'rda_dashboard_cap', 'manage_options' );
+	}
+
+	/**
+	 * Redirect URL
+	 *
+	 * @since 0.5
+	 *
+	 * @return string URL to redirect unauthorized users to.
+	 */
+	function redirect_url() {
+		return apply_filters( 'rda_redirect_url', home_url() );
 	}
 }
 
