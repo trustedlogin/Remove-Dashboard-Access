04bba2b8769aed9da8f2c628dede35d05c43e862
DrewAPicture <drew@10up.com>
2014-07-14 02:14:38 -0600

    Rework initialization from the main plugin file.

--- remove-wp-dashboard-access.php
+++ remove-wp-dashboard-access.php
@@ -3,19 +3,24 @@
  * Plugin Name: Remove Dashboard Access
  * Plugin URI: http://www.werdswords.com
  * Description: Removes Dashboard access for certain users based on capability.
- * Version: 1.0
+ * Version: 1.1
  * Author: Drew Jaynes (DrewAPicture)
  * Author URI: http://www.drewapicture.com
  * License: GPLv2
 */
 
-require_once( dirname( __FILE__ ) .	'/inc/class-rda-options.php' );       // RDA_Options Class
-require_once( dirname( __FILE__ ) . '/inc/class-rda-remove-access.php' ); // RDA_Remove_Access Class
+// RDA_Options Class
+require_once( dirname( __FILE__ ) . '/inc/class.rda-options.php' );
+
+// RDA_Remove_Access Class
+require_once( dirname( __FILE__ ) . '/inc/class.rda-remove-access.php' );
 
 // Load options instance
-if ( class_exists( 'RDA_Options' ) )
+if ( class_exists( 'RDA_Options' ) ) {
 	$load = new RDA_Options;
 
-// Run it
-if ( class_exists( 'RDA_Remove_Access' ) )
-	new RDA_Remove_Access( $load->capability(), $load->settings );
+	// Run it
+	if ( class_exists( 'RDA_Remove_Access' ) ) {
+		$access = new RDA_Remove_Access( $load->capability(), $load->settings );
+	}
+}
