77c4f2e0ea5cd6bde61b2eb30b00a1aa7eebfa86
DrewAPicture <drew@10up.com>
2016-03-15 23:19:37 -0600

    Properly scaffold tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,15 @@
 <?php
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../remove-dashboard-access.php';
+	require dirname( dirname( __FILE__ ) ) . '/remove-dashboard-access.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-
 require $_tests_dir . '/includes/bootstrap.php';
-
-activate_plugin( 'remove-dashboard-access/remove-dashboard-access.php' );
