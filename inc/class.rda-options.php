bcff711491e50b6b692562f16bad9a80abb863ab
Drew Jaynes <info@drewapicture.com>
2017-10-29 18:56:55 -0600

    Restore now-legacy files.

---
+++ inc/class.rda-options.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Legacy RDA_Options class file
+ *
+ * @since 1.0.0
+ * @deprecated 1.2.0 Use class-rda-options.php instead
+ *
+ * @package Remove_Dashboard_Access\Core
+ */
+
+_deprecated_file( __FILE__, '1.2.0', 'class-rda-options.php' );
+
+require_once __DIR__ . '/class-rda-options.php';
---
+++ inc/class.rda-remove-access.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Legacy RDA_Remove_Access class file
+ *
+ * @since 1.0.0
+ * @deprecated 1.2.0 Use class-rda-remove-access.php instead
+ *
+ * @package Remove_Dashboard_Access\Core
+ */
+
+_deprecated_file( __FILE__, '1.2.0', 'class-rda-remove-access.php' );
+
+require_once __DIR__ . '/class-rda-remove-access.php';
